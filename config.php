259cab79773ee5a8dff6f134c5c743c8f9f4753d
Alasdair Smith <alasdairsmith100@gmail.com>
2013-12-04 22:01:17 +0000

    Adds config file for pattern order, cos i'm just that ocd

---
+++ config.php
@@ -0,0 +1,31 @@
+<?php
+
+$config = [
+	'file_order' => [
+		'heading-1',
+		'heading-2',
+		'heading-3',
+		'heading-4',
+		'heading-5',
+		'heading-6',
+		'text',
+		'blockquote',
+		'list-ordered',
+		'list-unordered',
+		'form-text',
+		'form-textarea',
+		'form-select',
+		'form-checkbox',
+		'form-email',
+		'form-url',
+		'form-number',
+		'form-buttons',
+		'pagination-first',
+		'pagination-last',
+		'pagination',
+		'feedback',
+		'feedback-error',
+	]
+];
+
+?>
--- index.php
+++ index.php
@@ -6,46 +6,56 @@
 <link rel="stylesheet" href="global.css">
 <style>
 .pattern {
-    clear: both;
-    overflow: hidden;
+	clear: both;
+	overflow: hidden;
 }
 .pattern .display {
-    width: 65%;
-    float: left;
+	width: 65%;
+	float: left;
 }
 .pattern .source {
-    width: 30%;
-    float: right;
+	width: 30%;
+	float: right;
 }
 .pattern .source textarea {
-    width: 90%;
+	width: 90%;
 }
 </style>
 </head>
 <body>
 
 <?php
+include 'config.php';
+
 $files = array();
-$handle=opendir('patterns');
-while (false !== ($file = readdir($handle))):
-    if(stristr($file,'.html')):
-        $files[] = $file;
-    endif;
-endwhile;
-sort($files);
-foreach ($files as $file):
-    echo '<div class="pattern">';
-    echo '<div class="display">';
-    include('patterns/'.$file);
-    echo '</div>';
-    echo '<div class="source">';
-    echo '<textarea rows="6" cols="30">';
-    echo htmlspecialchars(file_get_contents('patterns/'.$file));
-    echo '</textarea>';
-    echo '<p><a href="patterns/'.$file.'">'.$file.'</a></p>';
-    echo '</div>';
-    echo '</div>';
-endforeach;
+$handle = opendir('patterns');
+
+while (false !== ($file = readdir($handle)))
+{
+	if (stristr($file, '.html'))
+	{
+		$files[] = $file;
+	}
+}
+
+foreach ($config['file_order'] as $filename)
+{
+	$file = $filename . '.html';
+	echo '<div class="pattern">';
+	echo 	'<div class="display">';
+				include('patterns/' . $file);
+	echo 	'</div>';
+
+	echo 	'<div class="source">';
+	echo 		'<textarea rows="6" cols="30">';
+	echo 		htmlspecialchars(file_get_contents('patterns/' . $file));
+	echo 		'</textarea>';
+
+	echo 		'<p><a href="patterns/' . $file . '">' . $file . '</a></p>';
+	echo 	'</div>';
+
+	echo '</div>';
+}
 ?>
 
 </body>
